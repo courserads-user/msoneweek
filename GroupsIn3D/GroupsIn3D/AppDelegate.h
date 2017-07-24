e567dc61698f665aaf68b4443ff3e3e2d4fbe42a
courserads-user <prashantbhide@live.com>
2017-07-24 11:13:50 -0700

    Adding hack xcode project

---
+++ GroupsIn3D/GroupsIn3D/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GroupsIn3D
+//
+//  Created by Prashant Bhide on 7/24/17.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GroupsIn3D/GroupsIn3D/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GroupsIn3D
+//
+//  Created by Prashant Bhide on 7/24/17.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
